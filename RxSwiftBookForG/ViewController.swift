511760ecb8b77ab360752a5d97b45b1c91c481b8
dai954 <da0606ha@gmail.com>
2021-09-24 17:53:01 +0900

    first commit

---
+++ RxSwiftBookForG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftBookForG
+//
+//  Created by 石川大輔 on 2021/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
