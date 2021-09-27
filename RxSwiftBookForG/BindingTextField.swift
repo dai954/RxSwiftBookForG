3a19b40d95f1ec6cfd79b4a0946b3cd4a452340c
dai954 <da0606ha@gmail.com>
2021-09-27 15:27:07 +0900

    Plus delgate patern

--- RxSwiftBookForG/BindingTextField.swift
+++ RxSwiftBookForG/BindingTextField.swift
@@ -7,35 +7,66 @@
 
 import UIKit
 
+//class BindingTextField: UITextField {
+//
+//    var textChangeClosure: (String) -> () = { _ in }
+//
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        CommonInit()
+//    }
+//
+//    required init?(coder: NSCoder) {
+//        super.init(coder: coder)
+//        CommonInit()
+//    }
+//
+//    fileprivate func CommonInit() {
+//        self.addTarget(self, action: #selector(textFieldChange), for: .editingChanged)
+//    }
+//
+//    func bind(callBack: @escaping (String) -> ()) {
+//
+//        self.textChangeClosure = callBack
+//
+//    }
+//
+//    @objc func textFieldChange(_ sender: UITextField) {
+//        if let text = sender.text {
+//            textChangeClosure(text)
+//        }
+//    }
+//
+//}
+
+
+protocol BindingTextFieldDelegate {
+    
+    func textFieldDidChange(text: String)
+    
+}
+
 class BindingTextField: UITextField {
     
-    var textChangeClosure: (String) -> () = { _ in }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        CommonInit()
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        CommonInit()
-    }
-    
-    fileprivate func CommonInit() {
-        self.addTarget(self, action: #selector(textFieldChange), for: .editingChanged)
-    }
-    
-    func bind(callBack: @escaping (String) -> ()) {
-        
-        self.textChangeClosure = callBack
-        
-    }
-    
-    @objc func textFieldChange(_ sender: UITextField) {
-        if let text = sender.text {
-            textChangeClosure(text)
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            commonInit()
         }
-    }
     
+        required init?(coder: NSCoder) {
+            super.init(coder: coder)
+            commonInit()
+        }
+    
+        private func commonInit() {
+            self.addTarget(self, action: #selector(textFieldChange), for: .editingChanged)
+        }
+    
+    
+        @objc func textFieldChange(_ textField: UITextField) {
+            
+            
+            
+        }
 }
 
