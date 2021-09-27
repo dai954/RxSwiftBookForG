5d7889087156ef0f40473a352631adccdba75f01
dai954 <da0606ha@gmail.com>
2021-09-27 14:57:26 +0900

    Binding Text Field

---
+++ RxSwiftBookForG/BindingTextField.swift
@@ -0,0 +1,41 @@
+//
+//  BindingTextField.swift
+//  RxSwiftBookForG
+//
+//  Created by 石川大輔 on 2021/09/27.
+//
+
+import UIKit
+
+class BindingTextField: UITextField {
+    
+    var textChangeClosure: (String) -> () = { _ in }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        CommonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        CommonInit()
+    }
+    
+    fileprivate func CommonInit() {
+        self.addTarget(self, action: #selector(textFieldChange), for: .editingChanged)
+    }
+    
+    func bind(callBack: @escaping (String) -> ()) {
+        
+        self.textChangeClosure = callBack
+        
+    }
+    
+    @objc func textFieldChange(_ sender: UITextField) {
+        if let text = sender.text {
+            textChangeClosure(text)
+        }
+    }
+    
+}
+
